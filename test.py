c281d40ab82ab64a403928d1c0f48e43a677e603
Kolawole Opatoye <kolawole.opatoye@gmail.com>
2021-07-27 22:27:43 +0100

    27-07-2021

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-class Person:
-    def __init__(self, name,age,tribe):
-        self.name = name
-        self.age = age
-        self.tribe = tribe
-
-    def greeting(self):
-        # Should return "hi, my name is " followed by the name of the Person.
-        return "hi, my name is {}. I am {} years old. I am {} by tribe".format(self.name, self.age, self.tribe)
-    def __str__(self):
-        return "This is {} greeting function".format(self.name)
-
-# Create a new instance with a name of your choice
-some_person = Person("Kolawole","23","yoruba")
-# Call the greeting method
-print(some_person.greeting())
-
-print(some_person)
-
+numbers = [1, 9, 8, 4, 7, 9]
+names = ["Mayor", "Kolawole", "Bose", "Adeola", "Folake"]
+print(sorted(names, key =len))
+numbers.sort()
+print(numbers)
+print(sorted(numbers))
